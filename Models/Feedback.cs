a3665fc94a96bd384c2d97613dcf487990c183fa
N0obitaz <charlesbernard.balaguer.041504@gmail.com>
2025-09-13 12:04:55 +0800

    Add project files.

---
+++ Models/Feedback.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication3.Models
+{
+    public class Feedback
+    {
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email format")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Address is required")]
+        public string Address { get; set; }
+
+        [Required(ErrorMessage = "Concern/Issues are required")]
+        public string Concern { get; set; }
+
+        [Required(ErrorMessage = "Please select a Department/Division")]
+        public string Department { get; set; }
+    }
+}
---
+++ Pages/Feedback.cshtml.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebApplication3.Models;
+using WebApplication3.Models;
+
+namespace WebApplication3.Pages
+{
+    public class FeedbackModel : PageModel
+    {
+        [BindProperty]
+        public Feedback Feedback { get; set; }
+
+        public string SuccessMessage { get; set; }
+
+        public void OnGet()
+        {
+        }
+
+        // SendForm
+        public IActionResult OnPostSend()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page(); 
+            }
+
+            SuccessMessage = "Feedback submitted successfully!";
+            return Page();
+        }
+
+
+        public IActionResult OnPostClear()
+        {
+            ModelState.Clear();
+            Feedback = new Feedback(); // reset form
+            return Page();
+        }
+    }
+}
